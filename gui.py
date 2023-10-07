0dada2513f59090ce4c166e2cfc4cdbbec19276c
Alex Gabbour <alex.gabbour@gmail.com>
2023-10-07 10:43:21 +0200

    added gui frames

--- gui.py
+++ gui.py
@@ -6,10 +6,13 @@ customtkinter.set_appearance_mode("system")
 customtkinter.set_default_color_theme("blue")
 
 root = customtkinter.CTk()
-root.geometry("750x400")
+root.geometry("1200x800")
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+menu = customtkinter.CTkFrame(master=frame)
+menu.pack(padx=(0, 300))
 
 label = customtkinter.CTkLabel(master=frame, text="hello")
 label.pack(pady=10, padx=12)
