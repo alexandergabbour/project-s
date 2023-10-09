5a88a898741673173fc4ae5243cc1fefb18f1e04
Alex Gabbour <alex.gabbour@gmail.com>
2023-10-09 18:34:44 +0200

    implemented class structure

--- gui.py
+++ gui.py
@@ -1,20 +1,43 @@
 #!/usr/bin/python3
 
+#from typing import Optional, Tuple, Union
+from typing import Optional, Tuple, Union
 import customtkinter
 
-customtkinter.set_appearance_mode("system")
-customtkinter.set_default_color_theme("blue")
+# customtkinter.set_appearance_mode("system")
+# customtkinter.set_default_color_theme("blue")
 
-root = customtkinter.CTk()
-root.geometry("1200x800")
 
-frame = customtkinter.CTkFrame(master=root)
-frame.pack(pady=20, padx=60, fill="both", expand=True)
+#outline of the interface
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        
+        self = customtkinter.CTk()
+        self.geometry("1200x800")
+        self.grid_columnconfigure((0, 1), weight=1)
 
-menu = customtkinter.CTkFrame(master=frame)
-menu.pack(padx=(0, 300))
+        self.menu = Menu(self)
+        self.menu.grid(row=0, column=0, padx=20, pady=20, sticky="nsw")
 
-label = customtkinter.CTkLabel(master=frame, text="hello")
-label.pack(pady=10, padx=12)
+        self.sc_cats = SC_cats(self)
+        self.sc_cats.grid(row=0, column=1, padx=20, pady=20, sticky="ew")
 
-root.mainloop()
+#menu contents
+class Menu(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        self.home = customtkinter.CTkButton(self, text="home")
+        self.home.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
+
+        self.favourites = customtkinter.CTkButton(self, text="favourites")
+        self.favourites.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")        
+
+#shortcuts categories contents
+class SC_cats(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+app = App()
+app.mainloop()
