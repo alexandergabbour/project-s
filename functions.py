df0ab2bae17351ee286071b74eaa2e7b1c08344c
Alex Gabbour <alex.gabbour@gmail.com>
2023-10-20 17:26:41 -0400

    ready for merge

--- functions.py
+++ functions.py
@@ -14,18 +14,37 @@ class pyScripts():
         x=3
         y=4
         print(x+y)
-
+    
     def subtract():
-        u=6
-        w=2
-        print(u-w)
+        x=7
+        y=2
+        print(x-y)
+
 
 class bashScripts():
 
     def script1(): #test function: bash stuff from user would go in the triple quotes below #! /bin/bash
         hello = '''
         #! /bin/bash
+        
         echo 'hello world'
-
         '''
         subprocess.call(['bash', '-c', hello])
+    
+    def linux_init():
+
+        #in this case 'init' stores the bash script. this could be stored in a seperate class and called by subprocess in the bashScripts class
+        init = '''
+        #! /bin/bash
+
+        echo 'initializing your linux machine...'
+
+        echo 'installing predefined software...'
+
+        sudo apt update && sudo apt upgrade
+
+        sudo snap install --classic code
+
+        sudo snap install spotify
+        '''
+        subprocess.call(['bash', '-c', init])
